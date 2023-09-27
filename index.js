7232069328b1180e227c82ba370ececbc5681a3f
Nikhil7282 <nikhil.sa2020ece@sece.ac.in>
2023-09-27 22:27:03 +0530

    db error

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const Port=process.env.PORT||8000
 
 const cors=require('cors')
 const express=require('express')
-const {dbConnect} = require('./auth/dbConfig')
+const {dbConnect} = require('./Routes/dbConfig')
 const passwordRouter=require('./Routes/passwordRoutes')
 const app=express()
 
 app.use(express.json())
 app.use(cors())
-// app.use(passwordRouter())
+app.use('/passwords',passwordRouter())
 
 
 
