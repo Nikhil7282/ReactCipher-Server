972221dc44d8914cc093b9157dfcd054ed33ca81
Nikhil7282 <nikhil.sa2020ece@sece.ac.in>
2023-09-28 22:18:18 +0530

    Password-Encryption

--- Routes/dbConfig.js
+++ Routes/dbConfig.js
@@ -1,12 +1,15 @@
-const mySql=require('mysql')
-const dbConnect=async()=>{
-    const db=mySql.createConnection({
+const mySql=require('mysql2')
+const db=mySql.createConnection({
         user:'root',
         host: 'localhost',
         password: 'root123',
         database:'password_manager'
     })
-    return db;
-}
-
-module.exports={dbConnect}
+    db.connect((err)=>{
+        if(err){
+            return console.log(err);
+        }
+        console.log("Connect to MySQL server");
+    })
+    // console.log(db);
+module.exports=db
--- Routes/passwordRoutes.js
+++ Routes/passwordRoutes.js
@@ -1,12 +1,15 @@
 const express=require('express')
+const {encrypt,decrypt}=require('../EncryptionHandler')
 const router=express.Router()
-const {dbConnect}=require('./dbConfig')
-const db=dbConnect()
+const db=require('./dbConfig')
+// console.log(db);
 
 router.post("/addPassword",(req,res)=>{
+    console.log(req.body);
     const {password,title}=req.body
-    db.query(`INSERT INTO passwords (password,title) VALUES (?,?)`,
-    [password,title],
+    const encryptedPassword=encrypt(password)
+    db.query("INSERT INTO passwords (password,title,iv) VALUES (?,?,?)",
+    [encryptedPassword.password,title,encryptedPassword.iv],
     (err,result)=>{
         if(err){console.log(err);}
         else{res.status(200).json({msg:"Success"})}
